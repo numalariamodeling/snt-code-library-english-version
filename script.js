c7a2e4ebbddb71f03ed5957d305f6393ae5f3563
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-10-22 11:12:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -44,10 +44,11 @@ As SNT matures, more quality assurance is needed such that NMCPs can be confiden
 
             <div class="round-buttons">
 
-                <button class="rect-button" onclick="window.location.href='https://';">View St FR</button>
                 <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-code-library-french-version/#shapefiles';">View R FR</button>
                 <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-python-french-version/#shapefiles';">View py FR</button>
                 <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-python-english-version/#shapefiles';">View py EN</button>
+                <button class="rect-button" onclick="window.location.href='https://';">View St FR</button>
+                <button class="rect-button" onclick="window.location.href='https://';">View St EN</button>
             </div>
             
             <h4 id="stepByStep">Step-by-step guide</h4>
